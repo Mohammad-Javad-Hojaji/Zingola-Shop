569282aa6e01adc6357d21457bdee197e16dae54
Mohammad Javad <mohammadjavad138998@gmail.com>
2026-05-06 07:32:43 -0700

    add poster 	I added a poster

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import Navbar from "../../components/Navbar/Navbar"
+import styled from "./Home.module.css"
+import poster from "./../../assets/image/my poster.png"
 function Home() {
     return (
         <>
-            <Navbar here="home"/>
+            <Navbar here="home" />
+            <div className={styled.poster_wrapper}>
+                <div className="container-md">
+                    <img src={poster} alt="poster site" />
+                </div>
+            </div>
         </>
     )
 }
