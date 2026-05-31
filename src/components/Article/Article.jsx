bcff3df8cc667d7791243d9953b0ade476c83c30
MohammadJavad <mohammadjavad138998@gmail.com>
2026-05-31 17:14:54 +0330

    I added Postes in My website 	you can see more articles 	and i need connect this part to the 	db.json .

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,17 @@
+export default function Article(props) {
+    return (
+        <div className="col-lg-3 col-sm-6 col-12  pt-3">
+            <div className="shadow p-3 mx-2 rounded article ">
+                <div className="text-center">
+                    <img src="images/Galaxys article.png" className="img-fluid rounded" alt="post" />
+                </div>
+                <h5 className="pt-4 color555 h6 text-center ">
+                    سری گوش های Galaxy سامسونگ
+                </h5>
+                <h6 className="color777 pt-2">
+                    در این مقاله شما با سری گوشی های Galaxy سامسونگ اشنا می شوید.
+                </h6>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,6 +16,7 @@ import Security from "./../../assets/image/security shopping.png"
 import Various_installments from "./../../assets/image/Various installments.png"
 import shoppingCard from "./../../assets/image/shopping card e.png"
 import HighVariety from "./../../assets/image/High variety.png"
+import Article from "../../components/Article/Article"
 
 function Home() {
     const [dRight, setDRight] = useState("none")
@@ -23,7 +24,7 @@ function Home() {
     const [right, setRight] = useState(0)
     const [postData, setPostData] = useState([])
     const [all, setAll] = useState(0)
-    const[categoryData,setCategoryData]=useState([])
+    const [categoryData, setCategoryData] = useState([])
 
 
 
@@ -34,7 +35,7 @@ function Home() {
             setAll(data.data.count)
 
         })
-        axios.get("http://localhost:8000/categories").then(data=>{
+        axios.get("http://localhost:8000/categories").then(data => {
             setCategoryData(data.data.CategoriesShow)
         })
     }, [])
@@ -91,7 +92,6 @@ function Home() {
                     </div>
                 </div>
             </div>
-
             <div className={styled.offer_products}>
                 <div className="container-md">
                     <h3 className="color555 py-5 mb-5 ms-2">پر تخفیف ترین محصولات</h3>
@@ -121,31 +121,44 @@ function Home() {
                     <h3 className="color555">دسته بندی ها</h3>
                     <div className="row  justify-content-around">
                         {
-                            categoryData.map(data=>{
-                                return(
-                                    <Category data={data} key={data.key}/>
+                            categoryData.map(data => {
+                                return (
+                                    <Category data={data} key={data.key} />
                                 )
 
                             })
-                        }          
+                        }
                     </div>
                 </div>
             </div>
-            <div>
-                <div className={styled.whyUs}>
-                    <div className="container-md my-5 pt-3">
-                        <h3 className="color555 mb-5">
-                            چرا زینگولا با دیگران متفاوت هست؟!
-                        </h3>
-                        <div className="row">
-                            <Card title={"امنیت در خرید"} pic={Security} alt={"security shopping"}/>
-                            <Card title={" اقساط متنوع "} pic={Various_installments} alt={"Various installments"}/>
-                            <Card title={"راحتی در خرید"} pic={shoppingCard} alt={"Shopping Card"}/>
-                            <Card title={"تنوع بالا"} pic={HighVariety} alt={"High Variety"}/>
-                        </div>
+            <div className={styled.whyUs}>
+                <div className="container-md my-5 pt-3">
+                    <h3 className="color555 mb-5">
+                        چرا زینگولا با دیگران متفاوت هست؟!
+                    </h3>
+                    <div className="row">
+                        <Card title={"امنیت در خرید"} pic={Security} alt={"security shopping"} />
+                        <Card title={" اقساط متنوع "} pic={Various_installments} alt={"Various installments"} />
+                        <Card title={"راحتی در خرید"} pic={shoppingCard} alt={"Shopping Card"} />
+                        <Card title={"تنوع بالا"} pic={HighVariety} alt={"High Variety"} />
                     </div>
                 </div>
             </div>
+
+            <div className={styled.lastPostes}>
+                <div className="container-md">
+                    <h3 className="color555">مقالات اخیر
+                        <span className="ps-3 h6 text-primary pointer">دیدن موارد بیشتر  &gt;</span>
+                    </h3>
+                    <div className="row justify-content-between">
+                        <Article />
+                        <Article />
+                        <Article />
+                        <Article />
+                    </div>
+                </div>
+            </div>
+
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </>
     )
