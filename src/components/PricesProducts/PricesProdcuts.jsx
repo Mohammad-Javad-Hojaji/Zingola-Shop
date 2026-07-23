8e54eaa256a80bb7a40b6ee1d11a4a490001a486
MohammadJavad <mohammadjavad138998@gmail.com>
2026-07-23 18:24:51 +0330

    feat:I added post for prices of products in different category 	I added post for prices of products in different category 	You can see a 4 item in different category in page of the Products.

---
+++ src/components/PricesProducts/PricesProdcuts.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import styled from "./pricesProdcuts.module.css"
+export default function PricesProdcuts(props) {
+    return (
+        <Link>
+            <div className={styled.w130}>
+                <div className={styled.PostOffer_wrapper}>
+                    <div className={styled.PostOffer_wrapper_img} >
+                        <div className="d-flex justify-content-center">
+                            <img src={`images/${props.pic}`} alt="" className="img-fluid" />
+                        </div>
+                    </div>
+                    <div>
+                        <h4 className="text-center color555 pb-3 px-3">{props.name} </h4>
+                        <p className="text-decoration-line-through text-center text-danger">{props.mainPrice}</p>
+                        <p className="text-center text-success pb-4">{props.offerPrice}</p>
+                    </div>
+                </div>
+
+            </div>
+        </Link>
+    )
+}
--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -6,6 +6,7 @@ import arrowright from "./../../assets/image/arrowright.png"
 import arrowleft from "./../../assets/image/arrowleft.png"
 import PostOffer from "../../components/PostOffer/PostOffer"
 import Category from "../../components/Category/Category";
+import PricesProdcuts from "../../components/PricesProducts/PricesProdcuts";
 
 export default function ProductsPage() {
     const [dRight, setDRight] = useState("none")
@@ -13,19 +14,29 @@ export default function ProductsPage() {
     const [right, setRight] = useState(0)
     const [postData, setPostData] = useState(null)
     const [all, setAll] = useState(0)
-    const [categoryData,setCategoryData]=useState(null)
+    const [categoryData, setCategoryData] = useState(null)
+    const [electronicData, setElectronicData] = useState(null)
+    const [poshakData, setPoshakData] = useState(null)
 
     useEffect(() => {
 
         axios.get("http://localhost:8000/offerProducts").then((data) => {
             setPostData(data.data.data);
             setAll(data.data.count)
-            
-            
+
+
         })
         axios.get("http://localhost:8000/categories").then((data) => {
             setCategoryData(data.data.CategoriesShow);
-            
+
+        })
+        axios.get("http://localhost:8000/products?categoryId=1&pageCategory=1").then(data => {
+            setElectronicData(data.data)
+
+
+        })
+        axios.get("http://localhost:8000/products?categoryId=2&pageCategory=1").then(data => {
+            setPoshakData(data.data)
         })
     }, [])
 
@@ -100,27 +111,84 @@ export default function ProductsPage() {
                     <h3 className="color555">دسته بندی ها</h3>
                     <div className="row  justify-content-around">
                         {
-                            categoryData?
-                            categoryData.map(data => {
-                                
-                                return (
-                                    <Category data={data} key={data.id} />
-                                )
+                            categoryData ?
+                                categoryData.map(data => {
 
-                            }):
-                            (
-                                <div className="d-flex justify-content-center py-5">
-                                    <div className="spinner-border" role="status">
-                                        <span className="sr-only"></span>
+                                    return (
+                                        <Category data={data} key={data.id} />
+                                    )
+
+                                }) :
+                                (
+                                    <div className="d-flex justify-content-center py-5">
+                                        <div className="spinner-border" role="status">
+                                            <span className="sr-only"></span>
+                                        </div>
                                     </div>
-                                </div>
 
-                            )
+                                )
                         }
                     </div>
                 </div>
             </div>
 
+            <div className={styled.productsPostsElectro}>
+                <div className="container-md pt-5 ">
+                    <h2 className="color555">
+                        لوازم الکترونیک
+                    </h2>
+                    <div className="pt-3">
+                        <div className="row ">
+                            {
+                                electronicData ? (
+                                    electronicData.map((value) => (
+                                        <div className="col-3">
+                                            <PricesProdcuts key={value.id} pic={value.pic} name={value.name} mainPrice={value.mainPrice} offerPrice={value.offerPrice} />
+                                        </div>
+                                    ))
+                                )
+                                    :
+                                    (
+                                        <div className="d-flex justify-content-center py-5">
+                                            <div className="spinner-border" role="status">
+                                                <span className="sr-only"></span>
+                                            </div>
+                                        </div>
+                                    )
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styled.productsPostsPoshak}>
+                <div className="container-md pt-5 ">
+                    <h2 className="color555">
+                        پوشاک
+                    </h2>
+                    <div className="pt-3">
+                        <div className="row">
+                            {
+                                poshakData ? (
+                                    poshakData.map((value) => (
+                                        <div className="col-3">
+                                            <PricesProdcuts key={value.id} pic={value.pic} name={value.name} mainPrice={value.mainPrice} offerPrice={value.offerPrice} />
+                                        </div>
+                                    ))
+                                )
+                                    :
+                                    (
+                                        <div className="d-flex justify-content-center py-5">
+                                            <div className="spinner-border" role="status">
+                                                <span className="sr-only"></span>
+                                            </div>
+                                        </div>
+                                    )
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </>
